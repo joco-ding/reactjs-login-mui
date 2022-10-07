07aa1d80342b9bd04e13754e35a511a858d9595d
Joko Nugroho <38367561+joco-ding@users.noreply.github.com>
2022-10-07 07:55:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const THEME = createTheme({
     "fontSize": 14,
     "fontWeightLight": 300,
     "fontWeightRegular": 400,
-    "fontWeightMedium": 500
+    "fontWeightMedium": 500,
+    "fontWeightBold": 700
   }
 });
 
